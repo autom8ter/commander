6a7f2e034a8b19baa2551d43e9114dc6761b04f4
autom8ter <admin@autom8ter.com>
2019-04-02 21:34:19 -0600

    init

---
+++ commander.go
@@ -0,0 +1,69 @@
+package commander
+
+import (
+	"context"
+	"fmt"
+	"github.com/autom8ter/util"
+	"github.com/pkg/errors"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+)
+
+type Author struct {
+	FullName string
+	Email    string
+}
+
+type Context struct {
+	Name       string    `json:"name"`
+	Download   string    `json:"download"`
+	Version    string    `json:"version"`
+	Authors    []*Author `json:"authors"`
+	ConfigPath string    `json:"config_path"`
+	EnvPrefix  string
+	Meta       map[string]string `json:"meta"`
+}
+
+var ctx = context.Background()
+
+var rootCmd = &cobra.Command{}
+var fs = &afero.Afero{
+	afero.NewOsFs(),
+}
+
+func Init(c *Context) {
+	rootCmd.Use = c.Name
+	c.Version = c.Version
+	rootCmd.Long = fmt.Sprintf("Context: \n%s\nCurrent Config: \n%s", util.ToPrettyJsonString(c), util.ToPrettyJson(viper.AllSettings()))
+	viper.SetFs(fs)
+	viper.SetEnvPrefix(c.EnvPrefix)
+	viper.SetConfigFile(c.ConfigPath)
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalln(err.Error())
+	}
+	log.Println(viper.ConfigFileUsed())
+}
+
+func Sub(name string, info string, fn func(ctx context.Context) error) {
+	newCtx := context.WithValue(ctx, "settings", viper.AllSettings())
+	rootCmd.AddCommand(&cobra.Command{
+		Use:  name,
+		Long: info,
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := fn(newCtx); err != nil {
+				log.Fatalln(errors.WithStack(err))
+			}
+		},
+	})
+}
+
+func Execute() error {
+	return rootCmd.Execute()
+}
+
+func FS() *afero.Afero {
+	return fs
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/autom8ter/commander
+
+require (
+	github.com/autom8ter/util v0.0.0-20190315143019-1ab48ff7769b
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/spf13/afero v1.2.2
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/viper v1.3.2
+	golang.org/x/net v0.0.0-20190324223953-e3b2ff56ed87 // indirect
+)
